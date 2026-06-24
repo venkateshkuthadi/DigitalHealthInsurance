a55755fe6a744b60004f7e22d72206e7c57cf0d2
JRahul14e <jadirahul551@gmail.com>
2026-06-24 15:56:31 +0530

    Hospital_entity_changes

---
+++ DigitalHealthInsurance/src/main/java/com/DigitalHealthinsurance/entity/HospitalEntity.java
@@ -0,0 +1,75 @@
+package com.DigitalHealthinsurance.entity;
+
+public class HospitalEntity {
+	private int hospital_id;
+	private String hospital_name;
+	private String city;
+	private String state;
+	private String address;
+	private String contact;
+	private String email;
+	private String specializaion;
+	private String cashless_available;
+	private String credited_at;
+	public int getHospital_id() {
+		return hospital_id;
+	}
+	public void setHospital_id(int hospital_id) {
+		this.hospital_id = hospital_id;
+	}
+	public String getHospital_name() {
+		return hospital_name;
+	}
+	public void setHospital_name(String hospital_name) {
+		this.hospital_name = hospital_name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSpecializaion() {
+		return specializaion;
+	}
+	public void setSpecializaion(String specializaion) {
+		this.specializaion = specializaion;
+	}
+	public String getCashless_available() {
+		return cashless_available;
+	}
+	public void setCashless_available(String cashless_available) {
+		this.cashless_available = cashless_available;
+	}
+	public String getCredited_at() {
+		return credited_at;
+	}
+	public void setCredited_at(String credited_at) {
+		this.credited_at = credited_at;
+	}
+
+}
