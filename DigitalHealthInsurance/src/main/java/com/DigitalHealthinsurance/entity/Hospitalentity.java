339353de2b0bed40608f3abf1d639c6a24e308a8
JRahul14e <jadirahul551@gmail.com>
2026-06-24 16:02:43 +0530

    Hospital_entity_chnages

---
+++ DigitalHealthInsurance/src/main/java/com/DigitalHealthinsurance/entity/Hospitalentity.java
@@ -0,0 +1,11 @@
+package com.DigitalHealthinsurance.entity;
+
+public class Hospitalentity {
+	private int id;
+	private String hospital_name;
+	private String city;
+	private String state;
+	private String addresss;
+	
+
+}
