4dbb5de1b8d4c4bb054118e4b712e081481696ea
JRahul14e <jadirahul551@gmail.com>
2026-06-24 16:13:14 +0530

    Changes_in_entity

---
+++ DigitalHealthInsurance/src/main/java/com/DigitalHealthinsurance/entity/hospitalentity.java
@@ -0,0 +1,40 @@
+package com.DigitalHealthinsurance.entity;
+
+public class hospitalentity {
+	private int id;
+	private String name;
+	private String city;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getSpecialization() {
+		return specialization;
+	}
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	private String address;
+	private String specialization;
+
+}
